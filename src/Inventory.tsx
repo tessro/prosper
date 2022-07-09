452db12e09acf0fa6053287b80bb16a90e7458c4
Paul Rosania <paul.rosania@gmail.com>
2022-07-08 23:41:09 -0700

    show price + value in inventory

--- src/Inventory.tsx
+++ src/Inventory.tsx
@@ -1,6 +1,7 @@
-import { useMemo, useState } from 'react';
+import { useContext, useMemo, useState } from 'react';
 
 import { FioClient, UserStorage, UserStorageItem } from './fio';
+import { OrderBookContext } from './contexts/OrderBookContext';
 
 async function fetchInventory(): Promise<UserStorage> {
   const client = new FioClient();
@@ -23,10 +24,21 @@ interface MaterialProps {
 }
 
 function Material({ ticker, quantity }: MaterialProps) {
+  const orderBook = useContext(OrderBookContext);
+  let cx;
+  if (orderBook) {
+    cx = orderBook.find(
+      (m) => m.MaterialTicker === ticker && m.ExchangeCode === 'IC1'
+    );
+  }
+
   return (
-    <li>
-      {quantity} {ticker}
-    </li>
+    <tr>
+      <td>{quantity}</td>
+      <td>{ticker}</td>
+      <td>{Math.round(100 * (cx?.PriceAverage ?? 0)) / 100}</td>
+      <td>{Math.round(quantity * 100 * (cx?.PriceAverage ?? 0)) / 100}</td>
+    </tr>
   );
 }
 
@@ -35,31 +47,47 @@ interface OtherProps {
 }
 
 function Other({ type }: OtherProps) {
-  return <li>{type}</li>;
+  return (
+    <tr>
+      <td colSpan={4}>{type}</td>
+    </tr>
+  );
 }
 
 function StorageLocation({ name, type, inventory }: StorageLocationProps) {
   return (
     <div>
-      <div>
+      <div className="my-2 font-bold">
         {name} ({type})
       </div>
-      {inventory.map((material) => {
-        if (material.Type === 'INVENTORY') {
-          return (
-            <Material
-              ticker={material.MaterialTicker}
-              name={material.MaterialName}
-              type={material.Type}
-              quantity={material.MaterialAmount}
-              weight={material.TotalWeight}
-              volume={material.TotalVolume}
-            />
-          );
-        } else {
-          return <Other type={material.Type} />;
-        }
-      })}
+      <table className="table bg-base-200">
+        <thead>
+          <tr>
+            <th>Qty</th>
+            <th>Mat.</th>
+            <th>Price</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {inventory.map((material) => {
+            if (material.Type === 'INVENTORY') {
+              return (
+                <Material
+                  ticker={material.MaterialTicker}
+                  name={material.MaterialName}
+                  type={material.Type}
+                  quantity={material.MaterialAmount}
+                  weight={material.TotalWeight}
+                  volume={material.TotalVolume}
+                />
+              );
+            } else {
+              return <Other type={material.Type} />;
+            }
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
