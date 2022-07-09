64846edf0a78354b3dffdfce5a7dae2ab8100631
Paul Rosania <paul.rosania@gmail.com>
2022-07-09 09:12:24 -0700

    add RecipeDatabase

---
+++ src/data/RecipeDatabase.ts
@@ -0,0 +1,36 @@
+import { Recipe, loadRecipes } from './fio';
+
+export class RecipeDatabase {
+  private readonly recipes: Recipe[];
+  private readonly byOutputTicker: Record<string, Recipe> = {};
+  private readonly byName: Record<string, Recipe> = {};
+
+  static default(): RecipeDatabase {
+    return new RecipeDatabase(loadRecipes());
+  }
+
+  constructor(recipes: Recipe[]) {
+    this.recipes = recipes.sort((a, b) =>
+      a.outputs[0].ticker.localeCompare(b.outputs[0].ticker)
+    );
+
+    for (const recipe of recipes) {
+      this.byName[recipe.name] = recipe;
+      for (const output of recipe.outputs) {
+        this.byOutputTicker[output.ticker] = recipe;
+      }
+    }
+  }
+
+  all(): Recipe[] {
+    return this.recipes;
+  }
+
+  findByOutput(ticker: string): Recipe | null {
+    return this.byOutputTicker[ticker] ?? null;
+  }
+
+  findByName(name: string): Recipe | null {
+    return this.byName[name] ?? null;
+  }
+}
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,5 @@
 export * from './BuildingDatabase';
 export * from './MaterialDatabase';
+export * from './RecipeDatabase';
 export * from './fio';
 export * from './workforce';
