ab04d928ba52f8ef63c53bb9d2d85ed17746b376
Paul Rosania <paul.rosania@gmail.com>
2022-07-08 22:05:20 -0700

    tighten margins

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -78,7 +78,7 @@ export function Sidebar({
 
   return (
     <div className="fixed z-10 bg-base-200 w-80 top-20 bottom-4 left-3 rounded-lg shadow p-4">
-      <div className="form-control my-2">
+      <div className="form-control">
         <label className="label" htmlFor="Sidebar/material">
           <span className="label-text">Material</span>
         </label>
