0449f3e191b883c7e02dcebf5b77d65580edb672
Paul Rosania <paul.rosania@gmail.com>
2022-07-10 16:27:59 -0700

    make FIO client available in console

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { OrderBookProvider } from './contexts/OrderBookContext';
+import { FioClient } from './data';
 import { Nav } from './Nav';
 import { SettingsPane } from './SettingsPane';
+
+(globalThis as any).fio = new FioClient();
 
 export default function App() {
   const [showSettings, setShowSettings] = useState(false);
